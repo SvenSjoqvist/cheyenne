9538b762f842b5b437910f5863e7e1d5a026225d
WebSnowie <165617978+WebSnowie@users.noreply.github.com>
2025-02-13 13:33:11 +0100

    u

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,8 +1,12 @@
 import { ReadonlyURLSearchParams } from "next/navigation";
 
-export function ensureStartWith(stringtocheck: string, startswith: string): string {
-    return stringtocheck.startsWith(startswith) ? stringtocheck : `${startswith}${stringtocheck}`;
+// utils.ts
+export function ensureStartWith(stringToCheck: string | undefined, startsWith: string): string {
+  if (!stringToCheck) {
+    throw new Error('String to check is undefined');
   }
+  return stringToCheck.startsWith(startsWith) ? stringToCheck : `${startsWith}${stringToCheck}`;
+}
 export function createUrl (pathname: string, params: URLSearchParams | ReadonlyURLSearchParams) {
     const paramsString = params.toString()
     const queryString = `${paramsString.length ? '?' : ''}${paramsString}`
