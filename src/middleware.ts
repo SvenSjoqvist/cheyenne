d2eb6f0054b705e24b43d2012617ec9e7a39076d
WebSnowie <165617978+WebSnowie@users.noreply.github.com>
2025-07-03 14:58:33 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
+
 export default withAuth(
   function middleware() {
     // Add security headers to all responses
