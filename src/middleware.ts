62113aee3320f2fa55e821fe8c8187ee12e1d696
WebSnowie <165617978+WebSnowie@users.noreply.github.com>
2025-07-04 11:51:35 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ export default withAuth(
   function middleware() {
     const response = NextResponse.next();
 
+    
     response.headers.set("X-Frame-Options", "DENY");
     response.headers.set("X-Content-Type-Options", "nosniff");
     response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
